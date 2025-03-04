c9412689d4e6d7d4d5ab4a35da4cac3e2750bfbf
Emmanuel Yee <127311695+Liquefy7822@users.noreply.github.com>
2025-03-04 09:52:31 +0800

    chore: convert next.config.ts to next.config.js

--- next.config.js
+++ next.config.js
@@ -1,7 +1,5 @@
-
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
+/** @type {import('next').NextConfig} */
+const nextConfig = {
   reactStrictMode: true,
   async rewrites() {
     return [
@@ -37,4 +35,4 @@ const nextConfig: NextConfig = {
   },
 };
 
-export default nextConfig;
+module.exports = nextConfig;
