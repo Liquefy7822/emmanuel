22fe11aa6b75ba8f05345f7b2fd65868dfc5190c
Emmanuel Yee <127311695+Liquefy7822@users.noreply.github.com>
2025-04-18 20:04:10 +0800

    lol

--- app/config.ts
+++ app/config.ts
@@ -4,13 +4,12 @@ export const metaData = {
   name: "Emmanuel Yee",
   ogImage: "/opengraph-image.png",
   description:
-    "A clean, fast, and lightweight portfolio template built with Next.js, Vercel, and Tailwind CSS for optimal performance.",
+    "Portfolio made by Emmanuel Yee, Templete from Vercel",
 };
 
 export const socialLinks = {
-  twitter: "https://x.com/1tssirius",
   github: "https://github.com/Liquefy7822",
-  instagram: "https://www.instagram.com/",
+  instagram: "https://www.instagram.com/emmanuelyeet/",
   linkedin: "https://www.linkedin.com/in/emmanuelyee/",
   email: "mailto:example@gmail.com",
 };
